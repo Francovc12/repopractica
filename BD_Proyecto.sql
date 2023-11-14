b0310943eeb47b683984e97d6e63262f6590041a
Franco Ignacio Villca <ignafrancov@gmail.com>
2023-11-14 20:36:26 -0300

    tabla usuario y datos de prueba

--- BD_Proyecto.sql
+++ BD_Proyecto.sql
@@ -5,5 +5,12 @@ CREATE TABLE IF NOT EXISTS Usuario(
     id_user INT(10) NOT NULL AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-
-)
+    nombre_completo VARCHAR(255) NOT NULL,
+    telefono INT(15) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id_user)
+);
+INSERT INTO Usuario VALUES(
+    1,'pepe10','123456','Jose Lopez',154211223,'pepe@gmail.com'),
+    (2,'diego30','012345','Diego Leal',155435321,'eldiego@gmail.com'
+);
